f13ebbe531e81cb44cc4209794f74f20c50afd7d
TsAcademyGroup4 <azuchukwuemeka9@gmail.com>
2026-04-23 14:29:41 -0700

    - fix: github actions fix for linting for JS

---
+++ frontend/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**']
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        setImmediate: 'readonly',
+        setInterval: 'readonly',
+        setTimeout: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'no-undef': 'warn',
+      'semi': ['warn', 'always'],
+      'quotes': ['warn', 'single'],
+      'indent': ['warn', 2]
+    }
+  }
+];
